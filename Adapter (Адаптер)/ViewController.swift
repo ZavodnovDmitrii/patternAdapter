c47748545135b28a8f0afa793dfc54f65727d784
Dmitrii Zavodnov <dmitrii.zavodnov@mail.ru>
2020-02-20 10:55:29 +0300

    Initial Commit

---
+++ Adapter (Адаптер)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adapter (Адаптер)
+//
+//  Created by Дмитрий Заводнов on 2/20/20.
+//  Copyright © 2020 Дмитрий Заводнов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
